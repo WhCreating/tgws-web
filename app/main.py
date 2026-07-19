24a4350fb3ef4d5cb28a675b0e3a073ac1a691f1
WhCreating <whcreating@github.com>
2026-07-19 22:38:59 +0500

    update v1.0.1

--- app/main.py
+++ app/main.py
@@ -6,12 +6,3 @@ from backend.routers import router as app_router
 app = FastAPI()
 
 app.include_router(app_router)
-
-
-@app.get("/")
-def start():
-    return RedirectResponse("/app")
-
-@app.get("/admin")
-def admin():
-    return RedirectResponse("/app/admin")
